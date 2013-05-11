a6aa35c002ca34f576706fc305e7001e2965a4cc
Colleen Josephson <cjoseph@mit.edu>
2013-05-11 16:44:13 -0400

    added udp client, weps.py

---
+++ raw/weps.py
@@ -0,0 +1,17 @@
+import socket
+import sys
+import random
+
+UDP_IP = sys.argv[1]
+UDP_PORT = 5001
+
+
+print("Spewing to {}:{}, {} bytes per packet".format(UDP_IP, UDP_PORT,
+                                                     MESSAGE_LEN))
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(UDP_IP, UDP_PORT)
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    print "recieved message", data 
