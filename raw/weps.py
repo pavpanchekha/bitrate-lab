75e501df5afdc85c65f0bf342b345cc5fe0b7062
Colleen Josephson <cjoseph@mit.edu>
2013-05-11 16:50:48 -0400

    bugfix weps again

--- raw/weps.py
+++ raw/weps.py
@@ -6,11 +6,10 @@ UDP_IP = sys.argv[1]
 UDP_PORT = 5001
 
 
-print("Spewing to {}:{}, {} bytes per packet".format(UDP_IP, UDP_PORT,
-                                                     MESSAGE_LEN))
+print("Listening on %r:%r." % (UDP_IP, UDP_PORT))
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(UDP_IP, UDP_PORT)
+sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024)
