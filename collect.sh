5d3008409a3d8bfd1878decf43b12fe15ed25b78
Pavel Panchekha <pavpanchekha@gmail.com>
2013-08-06 00:22:57 -0700

    Bash!

--- collect.sh
+++ collect.sh
@@ -5,7 +5,7 @@ FILE=$2
 SEC=$3
 ITER=$((2*SEC))
 
-if [ -f "raw/$FILE.trace" -o "$FILE" = "test"]; then
+if [ -f "raw/$FILE.trace" -o "$FILE" = "test" ]; then
     echo "File 'raw/$FILE.trace' already exists; will not overwrite"
     exit 1
 fi
