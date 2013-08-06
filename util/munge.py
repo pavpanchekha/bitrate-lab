293ed1f9c6997ea3bb4372d67b22d99c6b2de233
Pavel Panchekha <pavpanchekha@gmail.com>
2013-08-06 00:12:31 -0700

    Made munge ignore the sanity-check lines.

--- util/munge.py
+++ util/munge.py
@@ -9,16 +9,24 @@ Record = collections.namedtuple("Record", FIELDS)
 
 NUM_RATES = 12
 
+SANITY_RE = r"""\(\d+:\d+\s+\){""" + str(NUM_RATES) + "}"
+SANITY_RE = re.compile(SANITY_RE)
+
 LINE_RE = r"""Last\((\d+)\.(\d+)\) took (\d+) ns / (\d+) tries with rate (\d+) at (\d+)\((\d+)\) kbps \[(\d+)\]\n"""
 LINE_RE = re.compile(LINE_RE)
 def parse_line(lines):
     for line in lines:
         match_info = LINE_RE.match(line)
     
-        if not match_info:
-            print(ValueError("Cannot parse line", line))
-        else:
+        if match_info:
             yield Record(*map(int, match_info.groups()))
+            continue
+
+        match_info = SANITY_RE.match(line)
+        if match_info:
+            continue
+
+        print("Cannot parse line:", line, end="")
 
 def ts(record):
     return int(record.ts_s*1e9 + record.ts_ns)
