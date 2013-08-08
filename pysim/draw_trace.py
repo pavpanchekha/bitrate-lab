7c0295a8836ed468745766fecf785a7d1f3d5396
Pavel Panchekha <pavpanchekha@gmail.com>
2013-08-08 12:12:43 -0700

    Graph fixes for draw_trace
     + Scatter points to provide better estimate of density
     + Use opacity on points for better estimate of density
     + Order the bitrates by throughput, not rix

--- pysim/draw_trace.py
+++ pysim/draw_trace.py
@@ -5,6 +5,10 @@ import sys
 import matplotlib.cm as cm
 import rates
 import harness
+import random
+
+perm = {info.code: i for i, info in
+        enumerate(sorted(rates.RATES, key=lambda info: info.mbps))}
 
 def badness(rix, prob):
     txtime = harness.tx_time(rix, 1500)
@@ -38,8 +42,10 @@ if __name__ == "__main__":
         t = (i + .5) / width * (end - start) + start
         ps = [harness.packet_stats(rates, t, r) for r, _ in enumerate(rates)]
         badnesses = [badness(rix, p) / 1e6 for rix, p in enumerate(ps)]
-        best[i] = numpy.argmin(badnesses)
-        img[:, i] = ps
+
+        best[i] = perm[numpy.argmin(badnesses)]
+        for j, p in enumerate(ps):
+            img[perm[j], i] = p
 
     fig, ax = pylab.subplots()
     ax.set_xlim(0, width)
@@ -49,12 +55,19 @@ if __name__ == "__main__":
     ax.plot(range(width), best, 'g', linewidth=2)
 
     if len(sys.argv) > 2:
+        secs = (end - start) / 1e9
+        alpha = .5 ** (secs / 256)
+
         log = eval(open(sys.argv[2], "rt").read())
-        good_x = [(t - start) / (end - start) * width for t, rix, stat in log if stat]
-        good_y = [rix for t, rix, stat in log if stat]
-        bad_x  = [(t - start) / (end - start) * width for t, rix, stat in log if not stat]
-        bad_y  = [rix for t, rix, stat in log if not stat]
-        ax.plot(good_x, good_y, 'yo')
-        ax.plot(bad_x, bad_y, 'ro')
+        good_x = [(t - start) / (end - start) * width
+                  for t, rix, stat in log if stat]
+        good_y = [perm[rix] + .4*random.random() - .2
+                  for t, rix, stat in log if stat]
+        bad_x  = [(t - start) / (end - start) * width
+                  for t, rix, stat in log if not stat]
+        bad_y  = [perm[rix] + .4*random.random() - .2
+                  for t, rix, stat in log if not stat]
+        ax.plot(good_x, good_y, 'yo', alpha=alpha)
+        ax.plot(bad_x, bad_y, 'ro', alpha=alpha)
 
     pylab.show()
